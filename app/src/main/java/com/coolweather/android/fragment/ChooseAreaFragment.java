ef0ea83d26e95c7921686e20035ef48e97aeac1c
Brian <1786104060@qq.com>
2018-03-28 19:38:33 +0800

    修改应用名称和图标

--- app/src/main/java/com/coolweather/android/fragment/ChooseAreaFragment.java
+++ app/src/main/java/com/coolweather/android/fragment/ChooseAreaFragment.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -83,6 +84,8 @@ public class ChooseAreaFragment extends Fragment {
         recyclerView.setLayoutManager(linearLayoutManager);
         areaAdapter = new AreaAdapter(dataList);
         recyclerView.setAdapter(areaAdapter);
+        recyclerView.addItemDecoration(
+                new DividerItemDecoration(CoolWeatherApplication.getContext(), DividerItemDecoration.VERTICAL));
         return view;
     }
 
